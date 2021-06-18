9ccc9a76fd20b23379113a888f3554eddcacca2d
Florin Cristea <nfcristea@gmail.com>
2021-06-18 16:08:03 +0300

    Use rsync as cp lacks update flag on OSx.

--- src/setup.sh
+++ src/setup.sh
@@ -5,7 +5,7 @@ PRE_COMMIT_EXISTS=1
 (rm -f ../.git/hooks/pre-commit) || true
 
 mkdir -p ../.git/hooks/
-cp -u ./vendor/mariusmitrofanbostontr/phpcs-git-pre-commit/src/pre-commit ../.git/hooks/pre-commit
+rsync -u ./vendor/mariusmitrofanbostontr/phpcs-git-pre-commit/src/pre-commit ../.git/hooks/pre-commit
 chmod +x ../.git/hooks/pre-commit
 
 echo "Pre-commit git hook is updated!"
