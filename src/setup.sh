7b0f5d96f22f79dd8897130985687d507f337039
mariusmitrofanbostontr <71374670+mariusmitrofanbostontr@users.noreply.github.com>
2021-06-18 12:38:58 +0300

    Update setup.sh

--- src/setup.sh
+++ src/setup.sh
@@ -1,19 +1,11 @@
 #!/bin/sh
 
-if [ -e ../.git/hooks/pre-commit ];
-then
-    PRE_COMMIT_EXISTS=1
-else
-    PRE_COMMIT_EXISTS=0
-fi
+PRE_COMMIT_EXISTS=1
+
+(rm -f ../.git/hooks/pre-commit) || true
 
 mkdir -p ../.git/hooks/
 cp ./vendor/mariusmitrofanbostontr/phpcs-git-pre-commit/src/pre-commit ../.git/hooks/pre-commit
 chmod +x ../.git/hooks/pre-commit
 
-if [ "$PRE_COMMIT_EXISTS" = 0 ];
-then
-    echo "Pre-commit git hook is installed!"
-else
-    echo "Pre-commit git hook is updated!"
-fi
+"Pre-commit git hook is updated!"
