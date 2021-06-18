ee45acb6ad9d3297988660fd983807b34e285472
Florin Cristea <nfcristea@gmail.com>
2021-06-18 15:59:37 +0300

    Only copy if there are updates.

--- src/setup.sh
+++ src/setup.sh
@@ -5,7 +5,7 @@ PRE_COMMIT_EXISTS=1
 (rm -f ../.git/hooks/pre-commit) || true
 
 mkdir -p ../.git/hooks/
-cp ./vendor/mariusmitrofanbostontr/phpcs-git-pre-commit/src/pre-commit ../.git/hooks/pre-commit
+cp -u ./vendor/mariusmitrofanbostontr/phpcs-git-pre-commit/src/pre-commit ../.git/hooks/pre-commit
 chmod +x ../.git/hooks/pre-commit
 
 echo "Pre-commit git hook is updated!"
